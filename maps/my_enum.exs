212864375274403caecdb32af72b2a9d826a3a5b
Matt Engledowl <mengledowl@gmail.com>
2016-12-27 17:16:42 -0600

    Reimplement Enum.each

--- maps/my_enum.exs
+++ maps/my_enum.exs
@@ -1,4 +1,7 @@
 defmodule MyEnum do
   def all?([], _), do: true
   def all?([head | tail], func), do: func.(head) && all?(tail, func)
+
+  def each([], _), do: []
+  def each([head | tail], func), do: [func.(head) | each(tail, func)]
 end
