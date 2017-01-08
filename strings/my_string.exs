ebe9226ae1069292e53f4453b5a21cda08a2e0e9
Matt Engledowl <mengledowl@gmail.com>
2017-01-08 11:59:44 -0600

    Check if single-quoted string only contains ASCII chars

---
+++ strings/my_string.exs
@@ -0,0 +1,14 @@
+defmodule MyString do
+  # how I originially did it
+  def only_ascii_orig(list) do
+    sorted = Enum.sort(list)
+    [first, last] = [List.first(sorted), List.last(sorted)]
+    first >= ?\s && last <= ?~
+  end
+
+  # how I did it after refactoring to be more elixir-like
+  def only_ascii(list), do: Enum.sort(list) |> _is_only_ascii
+
+  defp _is_only_ascii([head | _]) when head < ?\s, do: false
+  defp _is_only_ascii(list), do: List.last(list) <= ?~
+end
