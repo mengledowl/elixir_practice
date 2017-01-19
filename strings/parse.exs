e8feec63b6c6fb309eb551142ff7a6bce0d54630
Matt Engledowl <mengledowl@gmail.com>
2017-01-18 18:33:56 -0600

    Center array of strings with each other

--- strings/parse.exs
+++ strings/parse.exs
@@ -10,4 +10,19 @@ defmodule Parse do
   defp _number_digits([non_digit | _], _) do
     raise "Invalid digit '#{[non_digit]}'"
   end
+
+  def center(list) do
+    max = Enum.map(list, &(String.length(&1))) |> Enum.max
+    _print(list, max)
+  end
+
+  defp _print([], _), do: []
+  defp _print([head | tail], length) do
+    lead = get_lead_pad(length, String.length(head))
+    String.pad_leading(head, lead + String.length(head), " ") |> IO.puts
+    _print(tail, length)
+  end
+
+  defp get_lead_pad(length, length), do: 0
+  defp get_lead_pad(length, h_length), do: div(length, h_length)
 end
