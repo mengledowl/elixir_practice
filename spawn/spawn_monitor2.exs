9eed2aeaf0834436a2207938ced4a8caa26f3416
Matt Engledowl <mengledowl@gmail.com>
2017-05-17 16:25:38 -0500

    Spawn link with raise & spawn monitor

---
+++ spawn/spawn_monitor2.exs
@@ -0,0 +1,31 @@
+defmodule SpawnMonitor2 do
+  import :timer, only: [sleep: 1]
+
+  def sad_function(sender) do
+    send(sender, "msg")
+    raise "boom"
+  end
+
+  def receive_msg do
+    receive do
+      msg ->
+        IO.puts "Message received: #{inspect(msg)}"
+        receive_msg
+      after 500 ->
+        IO.puts "No more messages!"
+    end
+  end
+
+  def run do
+    Process.flag(:trap_exit, true)
+    res = spawn_monitor(SpawnMonitor2, :sad_function, [self])
+
+    IO.inspect res
+
+    sleep 500
+
+    receive_msg
+  end
+end
+
+SpawnMonitor2.run
